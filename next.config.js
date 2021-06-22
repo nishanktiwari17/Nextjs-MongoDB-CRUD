af46d6c43b2e4a630156b65f9520dd576569fd9a
nishanktiwari17 <65443149+nishanktiwari17@users.noreply.github.com>
2021-06-22 15:59:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    MONGO_URI: "mongodb+srv://nishanktiwari17:Dranzer@12345@cluster0.buon9.mongodb.net/NotesDB?retryWrites=true&w=majority"
+    MONGO_URI: ""
   }
 }
